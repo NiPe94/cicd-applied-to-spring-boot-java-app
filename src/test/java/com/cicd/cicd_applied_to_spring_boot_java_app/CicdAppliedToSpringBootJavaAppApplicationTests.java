aff77cb64820b5894eeac278d5dda2492e24c337
Nightcrawler <nico.peter.123@web.de>
2019-11-08 11:46:06 +0100

    First Upload

---
+++ src/test/java/com/cicd/cicd_applied_to_spring_boot_java_app/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicd_applied_to_spring_boot_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAppliedToSpringBootJavaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
